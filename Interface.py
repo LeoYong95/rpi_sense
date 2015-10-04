474570a6ec5864101d88eceaeb0a0f225b72a3f4
LeoYong95 <y_c_l_@live.com>
2015-10-04 08:26:32 +0000

    Read Sensor and test interface

---
+++ Interface.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+w= Label(root, text="Hello World!!")
+
+w.pack()
+
+root.mainloop()
+
+
---
+++ ReadSensor.py
@@ -0,0 +1,79 @@
+#SENSE_PRO
+#         ______  ______  ____     ______  ______ 
+#        /  ___/ /  ___/ /    \   /  ___/ /  ___/ 
+#       /  /__  /  /__  /  /\  \ /  /__  /  /__ 
+#      /__   / /   __/ /  / /  //__   / /   __/
+#     ___/  / /   /__ /  / /  /___/  / /   /__ 
+#    /_____/ /______//__/ /__//_____/ /______/
+#
+#
+#
+#
+#Auth:LeoYong
+#Description:Read data from sensor
+#!/usr/local/bin/python
+#FIXME: add Timer function
+#       compile in a vnc file
+
+import time
+import RPi.GPIO as GPIO
+import csv
+import matplotlib.pyplot as plt
+from drawnow import *
+
+LDR=23
+GPIO.setmode(GPIO.BCM)
+
+class Sensor(object):
+        #-----------------------construct class
+        def __init__(self, Pin):
+            ##Return a new Read_Data object
+            self.Pin =Pin
+
+        #---------------------Read sensor reading
+        def Read_Analog(self):
+            reading = 0
+            #Discharge capacitor
+            GPIO.setup(self.Pin, GPIO.OUT)
+            GPIO.output(self.Pin, GPIO.LOW)
+            time.sleep(0.1)
+
+            GPIO.setup(self.Pin, GPIO.IN)
+            #Start Count
+            while(GPIO.input(self.Pin)==GPIO.LOW):
+                reading +=1
+
+            return reading
+
+        #------------------------Generate data CSV file 
+        def CSV_Gen(self,data,f):
+            dat = list()
+            dat.append(data)
+            wr = csv.writer(f)
+            for i in range(len(dat)):
+
+                wr.writerow([dat[i]])
+
+        #------------------Cuztomiz Plot.gplt
+        def Set_Plot(self,sensor):
+            Pltf1 = open("Plot.gplt",'r').read()
+            Pltf2 = open("Plot.gplt",'w')
+            m = Pltf1.replace("@sensor",sensor)
+            Pltf2.write(m)
+
+        #------------------PLot Graph       
+        def Plot_Dat(self):
+            from os import system
+            system('gnuplot Plot.gplt')
+
+
+light = Sensor(LDR)
+light.Set_Plot(str("Light"))#---HARD CODE
+
+with open('Light.csv','wb') as DataFile:
+        #----------Main Loop<this will not be an infinity loop FIXME>
+        while True:
+                data = light.Read_Analog() #Read fron the LDR
+                print data
+                light.CSV_Gen(data,DataFile)
+light.Plot_Dat()
