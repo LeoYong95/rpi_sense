9df4bc576436ddf8574d08be59edcfb9ad43d429
LeoYong95 <y_c_l_@live.com>
2015-10-06 12:40:00 +0000

    import matplotlib

--- ReadSensor.py
+++ ReadSensor.py
@@ -18,8 +18,9 @@
 import time
 import RPi.GPIO as GPIO
 import csv
-#import matplotlib.pyplot as plt
-#from drawnow import *
+import matplotlib.pyplot as plt
+import Plot_Test	
+from drawnow import *
 
 LDR=23
 GPIO.setmode(GPIO.BCM)
@@ -27,46 +28,44 @@ GPIO.setmode(GPIO.BCM)
 class Sensor(object):
         #-----------------------construct class
         def __init__(self, Pin):
-            ##Return a new Read_Data object
-            self.Pin =Pin
+                ##Return a new Read_Data object
+                self.Pin =Pin
 
         #---------------------Read sensor reading
         def Read_Analog(self):
-            reading = 0
-            #Discharge capacitor
-            GPIO.setup(self.Pin, GPIO.OUT)
-            GPIO.output(self.Pin, GPIO.LOW)
-            time.sleep(0.1)
+                reading = 0
+                #Discharge capacitor
+                GPIO.setup(self.Pin, GPIO.OUT)
+                GPIO.output(self.Pin, GPIO.LOW)
+                time.sleep(0.1)
 
-            GPIO.setup(self.Pin, GPIO.IN)
-            #Start Count
-            while(GPIO.input(self.Pin)==GPIO.LOW):
-                reading +=1
+                GPIO.setup(self.Pin, GPIO.IN)
+                #Start Count
+                while(GPIO.input(self.Pin)==GPIO.LOW):
+                        reading +=1
 
-            return reading
+                return reading
 
         #------------------------Generate data CSV file 
         def CSV_Gen(self,data,f):
-            dat = list()
-            dat.append(data)
-            wr = csv.writer(f)
-            for i in range(len(dat)):
+                dat = list()
+                dat.append(data)
+                wr = csv.writer(f)
+                for i in range(len(dat)):
 
-                wr.writerow([dat[i]])
+                        wr.writerow([dat[i]])
 
         #------------------Cuztomiz Plot.gplt
         def Set_Plot(self,sensor):
-            Pltf1 = open("Plot.gplt",'r').read()
-            Pltf2 = open("Plot.gplt",'w')
-            m = Pltf1.replace("@sensor",sensor)
-            Pltf2.write(m)
+                Pltf1 = open("Plot_Test",'r').read()
+                Pltf2 = open("Plot.gplt",'w')
+                m = Pltf1.replace("@sensor",sensor)
+                Pltf2.write(m)
 
         #------------------PLot Graph       
         def Plot_Dat(self):
-            from os import system
-            system('gnuplot Plot.gplt')
-
-
+                print "Plot data"
+            
 light = Sensor(LDR)
 light.Set_Plot(str("Light"))#---HARD CODE
 
@@ -76,4 +75,4 @@ with open('data/Light.csv','wb') as DataFile:
                 data = light.Read_Analog() #Read fron the LDR
                 print data
                 light.CSV_Gen(data,DataFile)
-light.Plot_Dat()
+                light.Plot_Dat()
